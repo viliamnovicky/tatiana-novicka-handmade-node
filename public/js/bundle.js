0a6f80e9ff3cc7e04594a65c917742d185b42a0a
viliamnovicky <viliamnovicky@gmail.com>
2023-05-31 10:58:35 +0200

    another try

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5549,7 +5549,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54763" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60387" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
